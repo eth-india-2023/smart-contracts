c1cd91ef10631a59ccebad71f56799112db49c4f
Soumalya Paul <samratghosh240@gmail.com>
2023-12-09 23:54:25 +0530

    Hardhat: Added SmartContracts of Share and Ticket

--- scripts/launch.js
+++ scripts/launch.js
@@ -34,11 +34,107 @@ async function ConnectaNftDeploy() {
         ])
     }
 }
+
+async function ConnectaTicketDeploy() {
+    const constructorParam = jsonContent.constructorParams
+
+    const ConnectaTicketFactory = await hre.ethers.getContractFactory("ConnectaTicket")
+    const ConnectaTicket = await ConnectaTicketFactory.deploy(
+        constructorParam.param1,
+        constructorParam.param2,
+        constructorParam.param3,
+        constructorParam.param4,
+        constructorParam.param5,
+        constructorParam.param6,
+        constructorParam.param7
+    )
+
+    await ConnectaTicket.deployed()
+    console.log("ConnectaTicket Deployed to: ", ConnectaTicket.address)
+
+    contractAddress = ConnectaTicket.address
+    blockNumber = ConnectaTicket.provider._maxInternalBlockNumber
+    
+    /// VERIFY
+    if (hre.network.name != "hardhat") {
+        await ConnectaTicket.deployTransaction.wait(6)
+        await verify(ConnectaTicket.address, [
+            constructorParam.param1,
+            constructorParam.param2,
+            constructorParam.param3,
+            constructorParam.param4,
+            constructorParam.param5,
+            constructorParam.param6,
+            constructorParam.param7
+        ])
+    }
+}
+
+async function ConnectaShareDeploy() {
+    const constructorParam = jsonContent.constructorParams
+    const ConnectaShareFactory = await hre.ethers.getContractFactory("ConnectaShare")
+    const ConnectaShare = await ConnectaShareFactory.deploy(
+        constructorParam.param1,
+        constructorParam.param2,
+        constructorParam.param3,
+        constructorParam.param4,
+        constructorParam.param5,
+        constructorParam.param6
+    )
+    await ConnectaShare.deployed()
+    console.log("ConnectaShare Deployed to:", ConnectaShare.address)
+    contractAddress = ConnectaShare.address
+    blockNumber = ConnectaShare.provider._maxInternalBlockNumber
+    /// VERIFY
+    if (hre.network.name != "hardhat") {
+        await ConnectaShare.deployTransaction.wait(6)
+        await verify(ConnectaShare.address, [
+            constructorParam.param1,
+            constructorParam.param2,
+            constructorParam.param3,
+            constructorParam.param4,
+            constructorParam.param5,
+            constructorParam.param6
+        ])
+    }
+}
+
+async function Token() {
+    const TokenFactory = await hre.ethers.getContractFactory("MyToken")
+    const token = await TokenFactory.deploy()
+
+    await token.deployed()
+
+    console.log("Token Deployed to:", token.address)
+    contractAddress = token.address
+    blockNumber = token.provider._maxInternalBlockNumber
+
+    /// VERIFY
+    if (hre.network.name != "hardhat") {
+        await token.deployTransaction.wait(6)
+        await verify(token.address, [])
+    }
+}
+
 async function main() {
     // ConnectaNft CONTRACT
     if (jsonContent.contractName == "ConnectaNft") {
         await ConnectaNftDeploy()
     }
+
+    if (jsonContent.contractName == "DreamStarterHolder") {
+        await ConnectaTicketDeploy()
+    }
+
+    if (jsonContent.contractName == "DreamStarterCollab") {
+        await ConnectaShareDeploy()
+    }
+
+    /// ERC20 
+    if (jsonContent.contractName == "Token") {
+        await Token()
+    }
+
 
     let chainId
 
